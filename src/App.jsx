7663498571a6258c7f692073bab23d6c7e4b7e27
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-07 18:41:56 +0300

    INstalled capacitors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
