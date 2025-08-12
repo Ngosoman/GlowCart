2e4f1b0a9f4322b7039d639403e674b3923cb626
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-12 11:35:37 +0300

    Added Logo

--- src/screens/Onboarding.jsx
+++ src/screens/Onboarding.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import logo from '../assets/logo.png'; // Make sure logo.png exists in assets
+import logo from '../assets/logo.png'; 
 
 const Onboarding = () => {
   const navigate = useNavigate();
